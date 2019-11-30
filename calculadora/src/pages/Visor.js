2a538a6657669e4dc8218cb58cfe73fb601c46de
maycon8609 <maycon8609@gmail.com>
2019-11-30 16:43:26 -0300

    componete Visor.js

---
+++ calculadora/src/pages/Visor.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class Visor extends React.Component {
+  render() {
+    return (
+      <div id="visor">
+        <h1>0</h1>
+      </div>
+    )
+  }
+}
